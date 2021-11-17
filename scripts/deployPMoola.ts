390dfb17ba7923e0a93e02f3682a96786ff9b3cf
0xLoaf <92998555+0xLoaf@users.noreply.github.com>
2021-11-17 11:50:15 -0500

    chore: add ci tests

--- scripts/deployPMoola.ts
+++ scripts/deployPMoola.ts
@@ -6,8 +6,19 @@ async function main() {
   console.log("Deploying the contracts with the address:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
+  // Mainnet
+  //   const Token = await ethers.getContractFactory("PMoolaToken");
+  //   const token = await Token.deploy(
+  //     "0x98A0c68d23275DcFE3f7b85f84c51b9e892a15Ad",
+  //     "0x6510f4477CD695AeB191092793309adE51e0D14D"
+  //   );
+  //   await token.deployed();
+
   const Token = await ethers.getContractFactory("PMoolaToken");
-  const token = await Token.deploy("0x98A0c68d23275DcFE3f7b85f84c51b9e892a15Ad");
+  const token = await Token.deploy(
+    "0x98A0c68d23275DcFE3f7b85f84c51b9e892a15Ad",
+    "0x769173eDb5572c0f4E77A3a6f5CE20784F986bba"
+  );
   await token.deployed();
 
   console.log("$PMOOLA deployed to:", token.address);
